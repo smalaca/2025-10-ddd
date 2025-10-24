1ccd82ea54f94a4e8cb26d4cfb85ceaa2ae1c237
smalaca <ab13.krakow@gmail.com>
2025-10-24 12:07:30 +0200

    Training - validate minimal and maximal attendees

--- src/main/java/com/smalaca/opentrainingsales/domain/training/TrainingFactory.java
+++ src/main/java/com/smalaca/opentrainingsales/domain/training/TrainingFactory.java
@@ -3,6 +3,18 @@ package com.smalaca.opentrainingsales.domain.training;
 // factory
 public class TrainingFactory {
     public Training create(AddNewTrainingDomainCommand command) {
+        if (command.minimalAttendees() < 5) {
+            throw new IllegalArgumentException("Minimal attendees must be at least 5");
+        }
+
+        if (command.maximalAttendees() > 20) {
+            throw new IllegalArgumentException("Maximal attendees must be at most 20");
+        }
+
+        if (command.maximalAttendees() < command.minimalAttendees()) {
+            throw new IllegalArgumentException("Maximal attendees must be greater than minimal attendees");
+        }
+
         return new Training(command);
     }
 }
